97f9c317342f667dd0a5729275450d507184c68c
unknown <qjpike@gmail.com>
2021-12-07 00:12:10 -0500

    add day 6

--- 6/main.py
+++ 6/main.py
@@ -14,17 +14,18 @@ def count_fish(cycles):
         fish[i] += 1
 
     for j in range(cycles):
-        new_fish = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0,8:0}
+        new_fish = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
         for i in fish:
             if i == 0:
                 new_fish[8] = fish[i]
                 new_fish[6] += fish[i]
             else:
-                new_fish[i-1] += fish[i]
+                new_fish[i - 1] += fish[i]
 
         fish = new_fish
 
     return sum(list(fish.values()))
 
-print("1:",count_fish(80))
-print("2:",count_fish(256))
+
+print("1:", count_fish(80))
+print("2:", count_fish(256))
